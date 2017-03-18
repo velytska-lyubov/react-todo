a15a673e13be06e8a56b169348e876b97753e789
Lyubov Velytska <velytska.lyubov@gmail.com>
2017-03-18 17:06:06 +0100

    First Commit

--- app/app.jsx
+++ app/app.jsx
@@ -2,17 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
 
 
 
 //Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css')
 $(document).foundation();
 
 //App css
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>Boilerplate project 3</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,13 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components',
+    ],
     alias: {
       applicationStyles: 'app/styles/app.scss',
-      
+
     },
     extensions: ['', '.js', '.jsx']
   },
