f3ff51ff4d73453e02e8f5dda78a2f74a1810226
Lyubov Velytska <velytska.lyubov@gmail.com>
2017-03-31 20:19:18 +0200

    Implement filterTodos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -43,18 +43,19 @@ var TodoApp = React.createClass ({
     this.setState({todos: updatedTodos});
   },
   handleSearch: function (showCompleted, searchText) {
-    this,setState ({
+    this.setState ({
       showCompleted: showCompleted,
       searchText: searchText.toLowerCase()
     });
   },
   render: function () {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
 
