815963af725c166d1d439bb2e3ef7f3bce0ec9de
Lyubov Velytska <velytska.lyubov@gmail.com>
2017-04-02 11:50:12 +0200

    Styling Todo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -26,7 +27,9 @@ var TodoApp = React.createClass ({
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
@@ -36,6 +39,7 @@ var TodoApp = React.createClass ({
     var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
 
       return todo;
@@ -54,9 +58,19 @@ var TodoApp = React.createClass ({
 
     return (
       <div>
-        <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-        <AddTodo onAddTodo={this.handleAddTodo}/>
+        <h1 className="page-title">Todo App</h1>
+
+        <div className="row">
+          <div className="column small-centered small-11 medium-6 large-5">
+            <div className="container">
+              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <AddTodo onAddTodo={this.handleAddTodo}/>
+            </div>
+          </div>
+        </div>
+
+
       </div>
 
     );
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,16 +19,21 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    //Expect createdAt to be a number
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
 
     it('should toggle completed value when handleToggle called', () => {
       var todoData = {
         id: 11,
         text: 'Test features',
-        completed: false
+        completed: false,
+        createAt: 0,
+        completedAt: undefined
       };
 
       var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
       todoApp.setState({todos: [todoData]});
+
 
       //check that todos first item has completed value of false.
       expect(todoApp.state.todos[0].completed).toBe(false);
@@ -36,6 +41,31 @@ describe('TodoApp', () => {
       todoApp.handleToggle(11);
       //Verify that value changed.
       expect(todoApp.state.todos[0].completed).toBe(true);
+      //Expect completedAt to be a number
+      expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    });
+//Test that when toggle from true to false, completedAt get removed
+    it('should toggle todo from completed to incompleted', () => {
+      var todoData = {
+        id: 11,
+        text: 'Test features',
+        completed: true,
+        createAt: 0,
+        completedAt: 123
+      };
+
+      var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+      todoApp.setState({todos: [todoData]});
+
+
+      //check that todos first item has completed value of false.
+      expect(todoApp.state.todos[0].completed).toBe(true);
+      //call handleToggle with 11,
+      todoApp.handleToggle(11);
+      //Verify that value changed.
+      expect(todoApp.state.todos[0].completed).toBe(false);
+      //Expect completedAt to be a number
+      expect(todoApp.state.todos[0].completedAt).toNotExist();
     });
   });
 });
